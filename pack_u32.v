f46a438f0e944ae85c01d0201cc75029dd9c1faf
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-04-07 11:43:57 -0700

    code fixes

--- pack_u32.v
+++ pack_u32.v
@@ -5,25 +5,35 @@ module pack_u32 (
 );
 
 logic [6:0] c0, c1, c2, c3, c4; // 7-bit chunks
-logic z1, z2, z3, z4; // zero flags
+logic [4:0] co; // or flags
+logic [4:0] gl; // glue bits
 
 // idata = $.wire()
 always_comb begin
   // c = idata.chop(7, {pad:0})
   {c4, c3, c2, c1, c0} = idata;
-  // z = $.map(c, e => e.or())
-  z1 = (|c1);
-  z2 = (|c2);
-  z3 = (|c3);
-  z3 = (|c4);
-  // len =
-  len = z4 ? 5 : (z3 ? 4 : (z2 ? 3 : (z1 ? 2 : 1)));
-  // o = $.map(c, (e, i) => z.high(i).or().glue(e))
-  o0 = {(z4 | z3 | z2 | z1), c0};
-  o1 = {(z4 | z3 | z2),      c1};
-  o2 = {(z4 | z3),           c2};
-  o3 = {z4,                  c3};
-  o4 = {1'b0,                c4};
+  // co = $.map(c, e => e.or())
+  co[0] = (|c0);
+  co[1] = (|c1);
+  co[2] = (|c2);
+  co[3] = (|c3);
+  co[4] = (|c4);
+  // gl = co.find()
+  gl[0] = co[4] | co[3] | co[2] | co[1];
+  gl[1] = co[4] | co[3] | co[2];
+  gl[2] = co[4] | co[3];
+  gl[3] = co[4];
+  gl[4] = 1'b0;
+  // len = gl.decode()
+  len[0] = gl[0] | gl[2] | gl[4];
+  len[1] = gl[1] | gl[2];
+  len[2] = gl[4] | gl[5];
+  // o = gl.right(c)
+  o0 = {gl[0], c0};
+  o1 = {gl[1], c1};
+  o2 = {gl[2], c2};
+  o3 = {gl[3], c3};
+  o4 = {gl[4], c4};
 end
 
 endmodule
