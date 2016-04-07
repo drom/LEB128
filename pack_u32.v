f37256255693e48e51d6fa7c6121924a1ebf7ce9
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-04-07 07:57:33 -0700

    Update pack_u32.v

--- pack_u32.v
+++ pack_u32.v
@@ -8,12 +8,16 @@ logic [6:0] c0, c1, c2, c3, c4; // 7-bit chunks
 logic z1, z2, z3, z4; // zero flags
 
 always_comb begin
+  // c = idata.chop(7, {pad:0})
   {c4, c3, c2, c1, c0} = idata;
+  // z = c.map(e => e.or())
   z1 = (|c1);
   z2 = (|c2);
   z3 = (|c3);
   z3 = (|c4);
+  // len =
   len = z4 ? 5 : (z3 ? 4 : (z2 ? 3 : (z1 ? 2 : 1)));
+  // o =
   o0 = {(z4 | z3 | z2 | z1), c0};
   o1 = {(z4 | z3 | z2),      c1};
   o2 = {(z4 | z3),           c2};
