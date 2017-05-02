4f8ed3c01d0db142b3632e7e12f6d1294698b9ca
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2017-05-02 18:04:22 +0200

    Use single input & generator for loop for 64 bits decoders

--- test/unpack_i64_tb.v
+++ test/unpack_i64_tb.v
@@ -3,47 +3,33 @@
 
 module unpack_i64_tb ();
 
-  reg  [ 7:0] i0, i1, i2, i3, i4, i5, i6, i7, i8, i9;
-  wire [63:0] o;
+  reg  [79:0] in;
+  wire [63:0] out;
   wire [ 3:0] len;
 
-  unpack_i64 DUT(i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, o, len);
+  unpack_i64 DUT(in, out, len);
 
   initial begin
     $dumpfile("unpack_i64_tb.vcd");
     $dumpvars(0, unpack_i64_tb);
 
-    i0 = 8'h01;
+    // Ignore unset bits after value
+    in = 80'h01xxxxxxxxxxxxxxxxxx;
     #1
-    `assert(o  , 1);
+    `assert(out, 1);
     `assert(len, 1);
 
-    i0 = 8'hff;
-    i1 = 8'hff;
-    i2 = 8'hff;
-    i3 = 8'hff;
-    i4 = 8'hff;
-    i5 = 8'hff;
-    i6 = 8'hff;
-    i7 = 8'hff;
-    i8 = 8'hff;
-    i9 = 8'h01;
+    // Decode negative numbers
+    in = 80'hffffffffffffffffff01;
     #1
-    `assert(o  , -1);
+    `assert(out, -1);
     `assert(len, 10);
 
-    i0 = 8'h80;
-    i1 = 8'h80;
-    i2 = 8'h80;
-    i3 = 8'h80;
-    i4 = 8'h0c;
-    i5 = 8'hbc;
-    i6 = 8'h0b;
-    i7 = 8'h00;
-    i8 = 8'h00;
-    i9 = 8'h00;
+    // Ignore data after value
+    in = 80'h808080800cbc0b000000;
+//    in = 80'h808080800cdeadbeef00;
     #1
-    `assert(o  , 32'hc0000000);
+    `assert(out, 32'hc0000000);
     `assert(len, 5);
 
     $display("ok");
--- unpack_u64.v
+++ unpack_u64.v
@@ -1,79 +1,43 @@
-module unpack_u64 (
-  input      [ 7:0] i0, i1, i2, i3, i4, i5, i6, i7, i8, i9,
-  output reg [63:0] o,
-  output reg [ 3:0] len
+module unpack_u64 #(
+  parameter N = 64
+)
+(
+  input      [  0:79] in,
+  output reg [N-1: 0] out,
+  output reg [  3: 0] len
 );
 
-// i = $.wire()
-reg [9:0] gl, ub, ho;
-reg [6:0] c0, c1, c2, c3, c4, c5, c6, c7, c8, c9;
-reg [6:0] k0, k1, k2, k3, k4, k5, k6, k7, k8, k9;
+  integer i;
+  reg [9:0] gl, ub, ho;
 
-always @* begin
-  // gl = i.msb() // glue bits
-  gl[0] = i0[7];
-  gl[1] = i1[7];
-  gl[2] = i2[7];
-  gl[3] = i3[7];
-  gl[4] = i4[7];
-  gl[5] = i5[7];
-  gl[6] = i6[7];
-  gl[7] = i7[7];
-  gl[8] = i8[7];
-  gl[9] = i9[7];
-  // c = i.lsb(7) // chunks
-  c0 = i0[6:0];
-  c1 = i1[6:0];
-  c2 = i2[6:0];
-  c3 = i3[6:0];
-  c4 = i4[6:0];
-  c5 = i5[6:0];
-  c6 = i6[6:0];
-  c7 = i7[6:0];
-  c8 = i8[6:0];
-  c9 = i9[6:0];
+  always @* begin
+    for(i=0; i<=9; i=i+1) begin
+      gl [i       ] = in[i*8       ];  // Glue bits
+      out[i*7 +: 7] = in[i*8+1 +: 7];  // Data chunks
+    end
 
-  // Used bytes
-  ub[0] = 1;
-  ub[1] =  gl[  0];
-  ub[2] = &gl[1:0];
-  ub[3] = &gl[2:0];
-  ub[4] = &gl[3:0];
-  ub[5] = &gl[4:0];
-  ub[6] = &gl[5:0];
-  ub[7] = &gl[6:0];
-  ub[8] = &gl[7:0];
-  ub[9] = &gl[8:0];
+    // Used bytes
+    ub[0] = 1;
 
-  // k = c.overwrite(dc, 0)
-  k1 = ub[1] ? c1 : 7'b0;
-  k2 = ub[2] ? c2 : 7'b0;
-  k3 = ub[3] ? c3 : 7'b0;
-  k4 = ub[4] ? c4 : 7'b0;
-  k5 = ub[5] ? c5 : 7'b0;
-  k6 = ub[6] ? c6 : 7'b0;
-  k7 = ub[7] ? c7 : 7'b0;
-  k8 = ub[8] ? c8 : 7'b0;
-  k9 = ub[9] ? c9 : 7'b0;
-  // o = k.glue()
-  o = {k9, k8, k7, k6, k5, k4, k3, k2, k1, c0};
+    for(i=1; i<=9; i=i+1)
+      ub[i] = gl[i-1] & ub[i-1];  // Should use more optimus &gl[i-1:0] instead
 
-  // Get high order byte
-  ho[0] = !ub[1] & ub[0];
-  ho[1] = !ub[2] & ub[1];
-  ho[2] = !ub[3] & ub[2];
-  ho[3] = !ub[4] & ub[3];
-  ho[4] = !ub[5] & ub[4];
-  ho[5] = !ub[6] & ub[5];
-  ho[6] = !ub[7] & ub[6];
-  ho[7] = !ub[8] & ub[7];
-  ho[8] = !ub[9] & ub[8];
-  ho[9] =          ub[9];
+    // Get high order byte
+    for(i=0; i<=8; i=i+1)
+      ho[i] = !ub[i+1] & ub[i];
 
-  len[0] = ho[0] |         ho[2] |         ho[4]         | ho[6]         | ho[8];
-  len[1] =         ho[1] | ho[2]                 | ho[5] | ho[6]                 | ho[9];
-  len[2] =                         ho[3] | ho[4] | ho[5] | ho[6];
-  len[3] =                                                         ho[7] | ho[8] | ho[9];
-end
+    ho[9] = ub[9];
+
+    // Generate decoded number
+    for(i=0; i<=9; i=i+1)
+      if(!ub[i])
+        out[i*7 +: 7] = 7'b0;
+
+    // Positional to binary
+    len[0] = ho[0] |         ho[2] |         ho[4]         | ho[6]         | ho[8];
+    len[1] =         ho[1] | ho[2]                 | ho[5] | ho[6]                 | ho[9];
+    len[2] =                         ho[3] | ho[4] | ho[5] | ho[6];
+    len[3] =                                                         ho[7] | ho[8] | ho[9];
+  end
 
 endmodule
